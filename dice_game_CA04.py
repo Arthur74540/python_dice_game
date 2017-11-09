121a49707a17cb4555aa8c701a194de84c32a0e0
Arthur74540 <32999208+Arthur74540@users.noreply.github.com>
2017-11-09 08:40:40 -0800

    Add files via upload

--- dice_game_CA04.py
+++ dice_game_CA04.py
@@ -1,10 +1,13 @@
 import random
 import sys
 
-def search(text, nb):
+def search_count(text, nb):
     count = counter.count(nb)
     if count > 0:
         print(str(count)+' '+text)
+        return True
+    else:
+        return False
 
 counter = []
 for i in range(7):
@@ -18,34 +21,38 @@ if len(sys.argv) > 1:
 for i in range(nb):
     dices.append(random.randint(1,6))
 
-def counter():
-    totalcounter = 0
-    for i in range(3):
-        if sorted(dices)[i] == sorted(dices[i+1]):
-            totalcounter = totalcounter + 1           
-
 print(dices)
 print(sorted(dices))
 
 for dice in dices:
     counter[dice] += 1
+print(counter)
+
+def search_run():
+    totalcounter = 0
+    for i in range(nb):
+        if counter[i] == counter[i+1] and counter[i] != 0:
+            totalcounter += 1
+    if search_count("Four the same", 4):
+        print("")
+    elif search_count("Three the same", 3):
+        print("")
+    elif totalcounter == 3:
+        print("Run of four")
+    elif totalcounter == 2 and search_count("Pair", 2):
+        print("Run of three with a pair")
+    elif search_count("Pair", 2):
+        print("")
+    elif totalcounter == 2:
+        print("Run of three")
+    elif totalcounter == 0 and search_count("Pair", 2):
+        print("Two pairs")
+    else:
+        print("All different")
 
 
-search("Four the same", 4)
-search("Three the same", 3)
-search("Pair", 2)
 
-counter()
-if totalcounter == 3:
-    print("Run of four")
-elif totalcounter == 2 and search("Pair", 2):
-    print("Run of three with a pair")
-elif totalcounter == 2:
-    print("Run of three")
-elif totalcounter == 0 and search("Pair", 2):
-    print("Two pairs")
-else:
-    print("All different")
+search_run()
 
 
     
